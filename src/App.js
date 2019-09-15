4e489737e9ad3baeff71501d049e7eea153d2bb4
张浩123 <13930333639@163.com>
2019-09-15 18:42:05 +0800

    路由基本使用

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+// 引入组件
+// 路由基本使用
+import BrowserRouter from './components/router.js';
 
 function App() {
     return (
         <div>
-            测试
+            <BrowserRouter></BrowserRouter>
         </div>
     );
 }
