280491910174ccd02cc6ca1cb86b3daf9af99482
张浩123 <13930333639@163.com>
2019-09-15 21:44:35 +0800

    动态路由，Switch中的exact记得加上

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { BrowserRouter } from 'react-router-dom';
 // 路由基本使用
 // import BrowserRouter1 from './components/router.js';
 // 重定向
-import Redirectcomponent from './components/04-Redirect.js';
+// import Redirectcomponent from './components/04-Redirect.js';
+// 动态路由
+import Dynamic from './components/05-dynamic.js';
 
 function App() {
     return (
         <BrowserRouter>
-            <Redirectcomponent></Redirectcomponent>
+            <Dynamic></Dynamic>
         </BrowserRouter>
     );
 }
